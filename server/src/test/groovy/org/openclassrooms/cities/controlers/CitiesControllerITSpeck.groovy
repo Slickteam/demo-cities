eb0dfdaa610bd67524fcd6722336da37de7023a1
jguidoux <jeremie.guidoux@gmaid.com>
2017-05-04 12:28:04 +0200

    create it test for controllers list cities

--- server/src/test/groovy/org/openclassrooms/cities/controlers/CitiesControllerITSpeck.groovy
+++ server/src/test/groovy/org/openclassrooms/cities/controlers/CitiesControllerITSpeck.groovy
@@ -1,7 +1,21 @@
-package org.openclassrooms.cities.controlers
+package org.openclassrooms.cities.controllers
+
+import org.openclassrooms.cities.utils.AbstractMvcSpec
+import org.springframework.http.HttpStatus
+import org.springframework.test.context.TestPropertySource
 
 /**
  * Created by jguidoux on 04/05/2017.
  */
-class CitiesControllerITSpeck {
+@TestPropertySource("classpath:/env-test.properties")
+class CitiesControllerITSpeck extends AbstractMvcSpec{
+
+    def "liste cities"() {
+        when:
+        def res = get('/cities/list')
+
+        then:
+        res.json.collect{it.name} == ["Paris", "Rennes", "Bordeaux", "Reims"]
+        res.status == HttpStatus.OK
+    }
 }

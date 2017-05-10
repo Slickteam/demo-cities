c3166e1ecebc8ec400c8df5d6a253d0dc1adae56
jguidoux <jeremie.guidoux@gmaid.com>
2017-05-10 18:51:37 +0200

    amélioratie error message when a city is not found

--- server/src/main/kotlin/org/openclassrooms/cities/exceptions/Exceptions.kt
+++ server/src/main/kotlin/org/openclassrooms/cities/exceptions/Exceptions.kt
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController
 
 
 @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "City Not Found") //404
-class CityNotFoundException(cityName: String?) : Exception("City $cityName not found.") {
+class CityNotFoundException(cityName: String?) : Exception("City '$cityName' not found.") {
 
     private val serialVersionUID = -3332292346834265371L
 }
